28fff6fa0a261598758cd70fba75444075a1953c
Mohaiminul Islam <36233551+mmislam1@users.noreply.github.com>
2023-12-11 20:32:36 +0600

    Update Challenge_1_solution_1.py

--- Challenge_1_solution_1.py
+++ Challenge_1_solution_1.py
@@ -2,17 +2,23 @@ def solution(s):
         answer=1
         piece=''
         
-        #loops through each character adding each to piece at a time. Piece determines the size of one share. process for each piece continues.
+        #loops through each character adding each to piece at a time. Piece determines the size of one share. process for each piece continues. Time complexity: O(n^2)
         
         for i in range(len(s)):
             piece+=s[i]
-            
+
+
+
+            # Below it checks if whole string is divisible by the selected piece. Leftover is not allowed!
+                
             if len(s)% len(piece)!=0:
                 continue
             flag=0 
             count=int(len(s)/len(piece))
             
-            
+
+            # Checks if that string can be divided into equal substrigs. Piece is matched all over the length of string. Time complexity: O(n)
+                
             for j in range(count):
                 if flag==1:
                     break
